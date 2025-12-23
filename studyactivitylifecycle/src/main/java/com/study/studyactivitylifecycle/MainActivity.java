feaba38a3586262610acf574bd428b430c69def2
DingJiaxiong <1612102142@qq.com>
2025-12-23 22:22:20 +0800

    13.5 Android 基础开发 - 四大组件入门 - Activity - Activity 关键生命周期

---
+++ studyactivitylifecycle/src/main/java/com/study/studyactivitylifecycle/MainActivity.java
@@ -0,0 +1,66 @@
+package com.study.studyactivitylifecycle;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Activity 生命周期";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "执行了 onCreate()");
+
+        Button button = findViewById(R.id.btn_finish);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "执行了 onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "执行了 onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "执行了 onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "执行了 onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "执行了 onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "执行了 onDestroy()");
+    }
+
+
+}
